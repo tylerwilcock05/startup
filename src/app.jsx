8745c3bb0c22626da517c82529215aebf4af4dd4
tylerwilcock05 <tylerwilcock05@gmail.com>
2026-02-12 19:51:57 -0700

    routes added to app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -16,29 +16,36 @@ export default function App() {
                 <div class="container-fluid">
                     <menu class="navbar-nav">
                         <li class="nav-item">
-                        <a class="nav-link title-menu" href="play.html"><b>Cracked Typer</b></a></li>
+                        <NavLink class="nav-link title-menu" to="play.html"><b>Cracked Typer</b></NavLink></li>
                         <li class="nav-item">
-                        <a class="nav-link" href="play.html">Play</a></li>
+                        <NavLink class="nav-link" to="play.html">Play</NavLink></li>
                         <li class="nav-item">
-                        <a class="nav-link" href="my-stats.html">My Stats</a></li>
+                        <NavLink class="nav-link" to="my-stats.html">My Stats</NavLink></li>
                         <li class="nav-item">
-                        <a class="nav-link" href="my-friends.html">My Friends</a></li>
+                        <NavLink class="nav-link" to="my-friends.html">My Friends</NavLink></li>
                         <li class="nav-item">
-                        <a class="nav-link" href="leaderboard.html">Leaderboard</a></li>
+                        <NavLink class="nav-link" to="leaderboard.html">Leaderboard</NavLink></li>
                         <li class="nav-item push-right">
-                        <a class="nav-link active" href="index.html">Login</a></li>
+                        <NavLink class="nav-link active" to="index.html">Login</NavLink></li>
                     </menu>
                 </div>
             </nav>
         </header>
   
-        <main>App components go here</main>
+        <Routes>
+            <Route path='/' element={<Login />} exact />
+            <Route path='/play' element={<Play />} />
+            <Route path='/leaderboard' element={<Leaderboard />} />
+            <Route path='/friends' element={<MyFriends />} />
+            <Route path='/stats' element={<MyStats />} />
+            <Route path='*' element={<NotFound />} />
+        </Routes>
   
         <footer class="bg-dark text-white-50 container-fluid py-3">
             <div class="container-fluid"> 
                 <span class="text-reset">Tyler Wilcock</span>
                 <button class="footer push-right">Change color scheme</button>
-                <a class="text-reset" href="https://github.com/tylerwilcock05/startup">GitHub</a>
+                <NavLink class="text-reset" to="https://github.com/tylerwilcock05/startup">GitHub</NavLink>
             </div>
     </footer>
       </div>
