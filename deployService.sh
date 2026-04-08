ac1893ecc0e8aa4ad6066797ed6bae30b115db22
“Tyler <tylerwilcock05@gmail.com>
2026-04-08 14:08:40 -0600

    package details edited to include ws

--- deployService.sh
+++ deployService.sh
@@ -24,6 +24,7 @@ npm run build # build the React front end
 cp -rf dist build/public # move the React front end to the target distribution
 cp service/*.js build # move the back end service to the target distribution
 cp service/*.json build
+cp service/package*.json build
 
 # Step 2
 printf "\n----> Clearing out previous distribution on the target\n"
