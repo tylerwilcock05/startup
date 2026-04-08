73e9cc6ee370957ea42521f28f3941c7c50fd02c
“Tyler <tylerwilcock05@gmail.com>
2026-04-08 01:10:16 -0600

    deployment bug fixed

--- service/index.js
+++ service/index.js
@@ -10,7 +10,7 @@ const { peerProxy } = require('./peerProxy.js');
 const authCookieName = 'token';
 
 // The service port. In production the front-end code is statically hosted by the service on the same port.
-const port = process.argv.length > 2 ? process.argv[2] : 3000;
+const port = process.env.PORT || (process.argv.length > 2 ? process.argv[2] : 3000);
 
 // JSON body parsing using built-in middleware
 app.use(express.json());
